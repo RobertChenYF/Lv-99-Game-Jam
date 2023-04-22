162e71b929faab22cf66d9c5e9652a908c08f5ee
Robert <yc3363@nyu.edu>
2023-04-22 23:15:31 +0800

    add lighting effects

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -19,7 +19,7 @@ public class Projectile : MonoBehaviour
         //计算两者之间的距离
         distanceToTarget = Vector3.Distance(this.transform.position, target);
         StartCoroutine(StartShoot());
-        Destroy(gameObject, 20);
+        //Destroy(gameObject, 20);
     }
     
     IEnumerator StartShoot()
@@ -30,11 +30,11 @@ public class Projectile : MonoBehaviour
             Vector3 targetPos = target;
  
             //让始终它朝着目标
-            this.transform.LookAt(targetPos);
+            //this.transform.LookAt(targetPos);
  
             //计算弧线中的夹角
             float angle = Mathf.Min(1, Vector3.Distance(this.transform.position, targetPos) / distanceToTarget) * 45;
-            this.transform.rotation = this.transform.rotation * Quaternion.Euler(Mathf.Clamp(-angle, -42, 42), 0, 0);
+            //this.transform.rotation = this.transform.rotation * Quaternion.Euler(Mathf.Clamp(-angle, -42, 42), 0, 0);
             float currentDist = Vector3.Distance(this.transform.position, target);
             if (currentDist < 0.01f)
                 move = false;
